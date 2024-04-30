aafdf4eaf99ef596c9cea5aa420ee2953d10394b
summerijo <mengotejianne@gmail.com>
2024-04-30 13:50:20 +0800

    first commit

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def realtime():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
